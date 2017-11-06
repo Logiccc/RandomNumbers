8625e9392f1d9cd331561f45d755bb414a3274da
Logiccc <te16.david.persson@umea.nti.se>
2017-11-06 10:53:29 +0100

    Ett program med slumpmässiga tal

--- src/TestIDE.java
+++ src/TestIDE.java
@@ -1,9 +1,28 @@
 /*
-    Test för IntelliJ
+    Slumpmässiga tal
     David Persson
     2017-11-06
  */
 
-public class TestIDE{
+import jdk.nashorn.internal.scripts.JO;
 
+import javax.swing.*;
+
+public class TestIDE{
+    public static void main(String[]args){
+        int abc = (int) (Math.random() * 100);
+
+        int tal = Integer.parseInt(JOptionPane.showInputDialog
+                (null, "Gissa ett tal mellan 1-100"));
+        if (tal == abc)
+            JOptionPane.showMessageDialog(null, "Du gissade rätt!");
+        else if (tal > abc)
+            JOptionPane.showMessageDialog(null, "FEL! DU gissade för för mycket");
+        else if (tal < abc)
+            JOptionPane.showMessageDialog(null, "FEL! DU gissade för lite");
+
+
+
+
+    }
 }
